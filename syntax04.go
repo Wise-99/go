53ad7ebccf49d77a5970b58086a8c60f65b92bbf
Yerin202044071 <yerenkim@gmail.com>
2021-09-14 22:19:56 +0900

    version 0.2

--- syntax04.go
+++ syntax04.go
@@ -6,10 +6,10 @@ import (
 	"time" // seed 생성용 패키지
 )
 
-난수 추출된 수의 소수 판정 프로그램 v0.1
-소수 : 1과 자기 자신 외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
+// 난수 추출된 수의 소수 판정 프로그램 v0.2
+// 소수 : 1과 자기 자신 외에는 나누어 떨어지지 않는 수 (0과 1은 제외)
 func main() {
-	seed 설정
+	// seed 설정
 	seed := time.Now().Unix()
 	rand.Seed(seed)
 
@@ -17,34 +17,16 @@ func main() {
 	number := rand.Intn(150) + 2 // 0과 1 제외, 2 ~ 151 사이의 수
 	fmt.Println("임의로 추출된 수 : ", number)
 
-	for i := 1; i <= number; i++ {
-		if number% i == 0 {
-			count++
+	for i := 2; i < number; i++ { // 1과 number일 때 loop 돌지 않음
+		if number%i == 0 {
+			//count++
+			count = count + 1
 		}
 	}
 
-	if count == 2 {
+	if count == 0 {
 		fmt.Println(number, "는 소수입니다!")
 	} else {
 		fmt.Println(number, "는 소수가 아닙니다!")
 	}
 }
-
-/*package main
-
-import (
-	"fmt"
-	"math/rand"
-	"time" // seed 생성용 패키지
-)
-
-func main() {
-	seed 설정
-	seed := time.Now().Unix()
-	rand.Seed(seed)
-
-	for i := 1; i < 6; i++ {
-		dice := rand.Intn(6) + 1 // int로 return, 0부터 입력한 숫자까지 return
-		fmt.Println(dice)
-	}
-}*/
